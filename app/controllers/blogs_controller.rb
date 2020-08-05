0eb7861e3f634b7a0744ee6820d0c6be55984a64
Reiko Enomoto <ooreicookoo@gmail.com>
2020-08-05 23:06:43 +0900

    WIP：ブログ

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,37 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
+  def index
+    @blogs = Blog.all
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
+  end
+  def edit
+  end
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+  def show
+  end
+  def destroy
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+end
